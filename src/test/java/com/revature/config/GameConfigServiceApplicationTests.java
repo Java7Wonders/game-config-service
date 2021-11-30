05246cd4519762e67067a7daa2005fde60ad9f67
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-11-30 16:06:53 -0500

    First commit

---
+++ src/test/java/com/revature/config/GameConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
